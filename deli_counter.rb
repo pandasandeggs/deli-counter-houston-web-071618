1a1e43bed75c6d893ac0590061747c877fba582d
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-07 16:05:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,7 @@ def now_serving(array)
 	if array.length > 0
 		array.each {|string| puts "#{string} is next in line."}
 		array.shift
-	else'
-		puts "There is nobody waiting to be served.
+	else
+	  
+		puts "There is nobody waiting to be served."
 end
