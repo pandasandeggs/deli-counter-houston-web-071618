59f8813895fcd56dd4aa631a2cd09b6f6b12a75e
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-07 16:10:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,11 @@ end
 def take_a_number(array,string)
 	array.each_with_index {|string,index| puts "Hello #{string}. You are number #{index+1} in line."}
 end
+
+def now_serving(array)
+	if array.length > 0
+		array.each {|string| puts "#{string} is next in line."}
+		array.shift
+	else
+		puts "There is nobody waiting to be served."
+end
