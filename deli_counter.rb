d4cd4cf641a4123ff9808b521c2d8743c263edc8
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-07 16:15:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
 	if array.length > 0
-	  array.each {|index,array| puts " The line is currently: #{index + 1}. #{array}"}
+	  array.each_with_index {|index,array| puts " The line is currently: #{index + 1}. #{array}"}
 	else
 	  puts "The line is currently empty."
 	 end
